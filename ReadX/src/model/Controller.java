2643684b91eb1c7c64b13e4382013c9b720809aa
Oscar Munoz Ramirez <1083870123@u.icesi.edu.co>
2023-05-05 14:45:27 -0500

    Update testInit requirements

--- ReadX/src/model/Controller.java
+++ ReadX/src/model/Controller.java
@@ -17,23 +17,30 @@ public class Controller {
 	 * This Bibliographic Class Arralist is for declare the program users
 	 */
 	ArrayList<Bibliographic> bibliographics;
+	private String msg;
+	private Random random;
+
+
 	/**
 	 * method builder of the Controller Class
 	 */
-	public Controller() {
+	public Controller(){
 		users = new ArrayList<User>();
 		bibliographics = new ArrayList<Bibliographic>();
+		msg = "";
+		random = new Random();
 	}
 
 	/**
 	 * This control method is for register the users, according if they are premium
 	 * or regular
+	 * 
 	 * @param name   user name
 	 * @param id     user id
 	 * @param option 1. User regular, 2. User Premium
 	 */
 	public String registerUser(String name, String id, int option) {
-		String msg = "The user is correctly recorded " + name;
+		msg = "The user is correctly recorded " + name;
 		// Generates the income date automatically
 		Calendar dateLinkage = Calendar.getInstance();
 		User user = null;
@@ -47,6 +54,7 @@ public class Controller {
 
 	/**
 	 * This control method is for search User by his id
+	 * 
 	 * @param id user id
 	 * @return User
 	 */
@@ -54,15 +62,17 @@ public class Controller {
 		User user = null;
 		boolean isFound = false;
 		for (int i = 0; i < users.size() && !isFound; i++) {
-			if (users.get(i).getID().equalsIgnoreCase(id)) {
+			if (users.get(i).getID().equalsIgnoreCase(id)||users.get(i).getName().equalsIgnoreCase(id)) {
 				isFound = true;
 				user = users.get(i);
 			}
 		}
 		return user;
 	}
+
 	/**
 	 * This control method create and add a new biblipgraphic product
+	 * 
 	 * @param option          Flag variable to know if 1.book or 2.Magazine is
 	 *                        created
 	 * @param codeId          Bibliographic product CodeId
@@ -79,14 +89,14 @@ public class Controller {
 	public String registerBibliographicProduct(int option, String name, int amountPag,
 			Calendar datePublication, String url, double value, int emission, int type, String review) {
 
+		msg = "The " + name + " product was registered sucessfully";
 		String codeId = generationAlfaAndHexaDecimal(option);
-		String msg = "The " + name + " product was registered sucessfully";
 		Bibliographic bibliographic = null;
 		switch (option) {
 			case 1 -> {
 				TypeBook typeBook = null;
-				typeBook=assignTypeBook(type);
-				bibliographic=new Book(codeId, name, amountPag, datePublication, url, value, type, review);
+				typeBook = assignTypeBook(type);
+				bibliographic = new Book(codeId, name, amountPag, datePublication, url, value, typeBook, review);
 				msg += "\nsuch as a Book ";
 			}
 			case 2 -> {
@@ -100,12 +110,13 @@ public class Controller {
 			}
 		}
 		this.bibliographics.add(bibliographic);
-		msg+=". Aditional, its code Hexadecimal is " + codeId;
+		msg += ". Aditional, its code Hexadecimal is " + codeId;
 		return msg;
 	}
 
 	/**
 	 * This method is for assign the type that is the Magazine
+	 * 
 	 * @param type varaible flag to indicate the type
 	 * @return The Magazine type
 	 */
@@ -121,6 +132,7 @@ public class Controller {
 
 	/**
 	 * This method is for assign the type emission of the Magazine
+	 * 
 	 * @param type varaible flag to indicate the typeEmission
 	 * @return The type Emission
 	 */
@@ -148,6 +160,7 @@ public class Controller {
 
 	/**
 	 * This control method is for search the bibliographicProduct by its name
+	 * 
 	 * @param wordKey bibliographicProduct name or identification id
 	 * @return bibliographicProduct, if it exist
 	 */
@@ -167,22 +180,23 @@ public class Controller {
 	/**
 	 * This control method is for generate a unique code Hexadecimal or alphanumeric
 	 * to bibliographic product
+	 * 
 	 * @param option Type product--1.Book (codeHexa)--2.Magazine(codeAlphanumeric)
 	 * @return the code identification of the product
 	 */
 	public String generationAlfaAndHexaDecimal(int option) {
-		String msg="",hexDigits = "0123456789ABCDEF", alphaDigits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String hexDigits = "0123456789ABCDEF", alphaDigits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String msg = "";
 		boolean exist = false;
-		Random random = new Random();
 		String source = (option == 1) ? hexDigits : alphaDigits;
-		String codeId="";
+		String codeId = "";
 		do {
 			msg = "";
 			for (int i = 0; i < 3; i++) {
 				msg += source.charAt(random.nextInt(source.length()));
 			}
 			for (int i = 0; i < bibliographics.size() && !exist; i++) {
-				codeId=bibliographics.get(i).getCodeId();
+				codeId = bibliographics.get(i).getCodeId();
 				if (codeId.equalsIgnoreCase(msg)) {
 					exist = true;
 				}
@@ -194,6 +208,7 @@ public class Controller {
 	/**
 	 * This control method is for decide the type intance the of bibliographic
 	 * product
+	 * 
 	 * @param wordKey bibliographicProduct name or identification id
 	 * @return 1.Book intance or 2.Magazine Intance
 	 */
@@ -211,7 +226,8 @@ public class Controller {
 
 	/**
 	 * This control method create and add a new biblipgraphic product
-	 * @param wordKey		  Bibliographic product codeIdentification or name
+	 * 
+	 * @param wordKey         Bibliographic product codeIdentification or name
 	 * @param name            Bibliographic product name
 	 * @param amountPag       Bibliographic product amount pages
 	 * @param datePublication Bibliographic product date publication
@@ -219,77 +235,132 @@ public class Controller {
 	 * @param value           Bibliographic product value of the price or
 	 *                        subscrition of the product
 	 * @param emission        The amount of duration in the emission of the magazine
-	 * @param typeProduct            varFlag for know Book type or Mazazine type
+	 * @param typeProduct     varFlag for know Book type or Mazazine type
 	 * @return
 	 */
 	public String modifiedProductBibliographic(String wordKey, String name, int amountPag, Calendar datePublication,
-			String url,double value, int emission,
-			 int typeProduct) {
+			String url, double value, int emission, int typeProduct) {
 
-		String msg = "";
+		msg = "";
 		Bibliographic product = null;
 		product = searchBibliographic(wordKey);
 
 		if (!(name.equals(""))) {
 			product.setName(name);
-			msg+="\n Name \2";
+			msg += "\n Name \2";
 		}
 		if (amountPag != 0) {
 			product.setAmountPag(amountPag);
-			msg+="\n Amount page \2";
+			msg += "\n Amount page \2";
 		}
 		if (datePublication != null) {
 			product.setDatePublication(datePublication);
-			msg+="\n Date publication \2";
+			msg += "\n Date publication \2";
 		}
 		if (value != 0) {
 			product.setValue(value);
-			msg+="\n Product or suscription product \2";
+			msg += "\n Product or suscription product \2";
 		}
-		if(url!=""){
+		if (!(url.equals(""))) {
 			product.setUrl(url);
-			msg+="\n Url \2";
+			msg += "\n Url \2";
 		}
-		if(emission!=0){
-			Emission typeEmission=null;
-			typeEmission=assignTypeEmission(emission);
-			((Magazine)product).setEmission(typeEmission);
-			msg+="\n Emission \2";
+		if (emission != 0) {
+			Emission typeEmission = null;
+			typeEmission = assignTypeEmission(emission);
+			((Magazine) product).setEmission(typeEmission);
+			msg += "\n Emission \2";
 		}
 		if (typeProduct != 0) {
 			if (product instanceof Book) {
 				TypeBook typeBook = null;
-				typeBook=assignTypeBook(typeProduct);
+				typeBook = assignTypeBook(typeProduct);
 				((Book) product).setType(typeBook);
-				
+
 			} else if (product instanceof Magazine) {
 				TypeMagazine typeMagazine = null;
 				typeMagazine = assignTypeMagazine(typeProduct);
 				((Magazine) product).setType(typeMagazine);
 			}
-			msg+="\n Type product \2";
+			msg += "\n Type product \2";
 		}
-		if(!(msg.equals(""))){
-			msg="Update sucessfully of product: "+msg;
-		}else{
-			msg="There are no changes";
+		if (!(msg.equals(""))) {
+			msg = "Update sucessfully of product: " + msg;
+		} else {
+			msg = "There are no changes";
 		}
-		return msg+"\n";
+		return msg + "\n";
 	}
+
 	/**
 	 * This cotrol method delete the me
+	 * 
 	 * @param wordKey
 	 */
 	public String deleteProduct(String wordKey) {
-		boolean isFound=false;
-		Bibliographic product=searchBibliographic(wordKey);
+		Bibliographic product = searchBibliographic(wordKey);
 		bibliographics.remove(product);
-		return "The "+wordKey+" was delete";
+		return "The " + wordKey + " was delete";
 	}
 
+	/**
+	 * This Control Method performs a start test to create 10 regular users and premium, also for books and magazines
+	 * @return The information of the object create
+	 */
 	public String testInit() {
-		// TODO - implement Controller.testInit
-		throw new UnsupportedOperationException();
+		msg = "";
+		int j = 0;
+		int amountPag = 0, type = 0, emission = 0;
+		String product = "", name = "", url = "", review = "NN", id = "";
+		double productValue = 0;
+		Calendar date = Calendar.getInstance();
+		Bibliographic bibliographic = null;
+		User user = null;
+
+		for (j = 0; j < 10; j++) {
+			name = "Regular " + (j);
+			id = String.valueOf(random.nextInt(Integer.MAX_VALUE));
+			user = new Regular(name, id, date);
+			users.add(user);
+			msg += "\4User: " + name + "\tid: " + id + "\n";
+
+			name = "Premium " + j;
+			id = String.valueOf(random.nextInt(Integer.MAX_VALUE));
+			user = new Premium(name, id, date);
+			users.add(user);
+			msg += "\4User: " + name + "\tid: " + id + "\n";
+
+			for (int i = 1; i <= 2; i++) {
+				product = (i == 1) ? "Book" : "Magazine";
+				name = product + " " + j;
+				amountPag = random.nextInt(1000) + 1;
+				type = random.nextInt(3) + 1;
+				id = generationAlfaAndHexaDecimal(i);
+				url = generationAlfaAndHexaDecimal(i);
+				url = url + ".PNG";
+				productValue = 10000 + random.nextInt(100000);
+				// Generates dates since 1900
+				int day = random.nextInt(31) + 1;
+				int month = random.nextInt(12) + 1;
+				int year = random.nextInt(123) + 1900;
+				date.set(year, month, day);
+				if (i == 1) {
+					TypeBook typeBook = assignTypeBook(type);
+					bibliographic = new Book(id, name, amountPag, date, url, productValue, typeBook, review);
+				} else {
+					emission = random.nextInt(4) + 1;
+					Emission typeEmission = assignTypeEmission(emission);
+					TypeMagazine typeMagazine = assignTypeMagazine(type);
+					bibliographic = new Magazine(id, name, amountPag, date, url, productValue, typeEmission,
+							typeMagazine);
+				}
+				msg +="\4"+ name + "->  code: " + id+"\n";
+				bibliographics.add(bibliographic);
+			}
+			msg+="\n\n";
+		}
+
+		return msg;
 	}
 
 	/**
